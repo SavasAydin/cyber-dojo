0334c333b86ad4b45b179168e1f35bd07d938dba
savas <savasaydin@gmail.com>
2013-07-04 21:44:43 +0200

    refactored get_number_names/1 and divided into two smaller funs

--- number_names/src/number_names.erl
+++ number_names/src/number_names.erl
@@ -2,38 +2,45 @@
 
 -export([spell_out/1]).
 
-spell_out(Number) ->
-    string:strip(get_number_name(Number)).    
+-define(BASE_TEN, 10).
 
-get_number_name(Number) ->
-    case is_irregular(Number) of 
-	true ->
-	    get_irregular_name(Number);
-	false ->
-	    {Tens, Ones} = parse_base_ten(Number),
-	    get_tens_name(Tens) ++ get_ones_name(Ones)
-    end.
+spell_out(Number) ->
+    NumberNameFun = get_number_fun(Number),
+    NumberName = NumberNameFun(Number),
+    string:strip(NumberName).
+
+get_number_fun(Number) ->
+    proplists:get_value(is_irregular(Number), number_name_funs()).
+			 
+is_irregular(Number) ->
+    none =/= proplists:lookup(Number, irregular_number_names()).
+
+number_name_funs() ->
+    [{false, fun get_regular_number_name/1},
+     {true, fun get_irregular_number_name/1}
+    ].
+
+get_regular_number_name(Number) ->
+    {Tens, Ones} = parse_base_ten(Number),
+    get_tens_name(Tens) ++ get_ones_name(Ones).
 
 parse_base_ten(Number) ->
     {get_tens_place(Number), get_ones_place(Number)}.
   
 get_tens_place(Number) ->  
-    Number div 10.
+    Number div ?BASE_TEN.
 
 get_ones_place(Number) ->
-    Number rem 10.
-
-is_irregular(Number) ->
-    none =/= proplists:lookup(Number, irregular_number_names()).
-
-get_irregular_name(Number) ->
-    proplists:get_value(Number, irregular_number_names()).
+    Number rem ?BASE_TEN.
 
 get_tens_name(Number) ->
     proplists:get_value(Number, tens_names()).
 
 get_ones_name(Number) ->
     proplists:get_value(Number, ones_names()).
+
+get_irregular_number_name(Number) ->	
+    proplists:get_value(Number, irregular_number_names()).
 
 irregular_number_names() ->
     [{0, "zero"},
