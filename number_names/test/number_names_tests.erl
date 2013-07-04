522193a87f272d01bfaf6b02febd078742ce88f7
savas <savasaydin@gmail.com>
2013-07-04 22:02:07 +0200

    altered way of test execution

--- number_names/test/number_names_tests.erl
+++ number_names/test/number_names_tests.erl
@@ -2,9 +2,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-
 spell_out_a_number_test_() ->
-    
     [assert_number("zero", 0),
      assert_number("one", 1),
      assert_number("two", 2),
@@ -35,7 +33,9 @@ spell_out_a_number_test_() ->
     ].
 
 assert_number(NumberName, Number) ->
-    ?_assertEqual(NumberName, number_names:spell_out(Number)).
+    fun() ->
+	    ?assertEqual(NumberName, number_names:spell_out(Number))
+    end.
 
 
 
