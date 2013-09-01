c71dc1d46006911ddcc576785dbb0f1dab44e7ef
savas <savasaydin@gmail.com>
2013-09-01 20:19:58 +0200

    added missing test case

--- tennis/test/tennis_tests.erl
+++ tennis/test/tennis_tests.erl
@@ -8,6 +8,15 @@ score_players_when_game_starts_test() ->
 score_when_first_player_scores_one_test() ->
     ?assertEqual({"fifteen", "love"}, tennis:score(1,0)).
 
+score_when_first_player_scores_two_test() ->
+    ?assertEqual({"thirty", "love"}, tennis:score(2,0)).
+
+score_when_first_player_scores_three_test() ->
+    ?assertEqual({"forty", "love"}, tennis:score(3,0)).
+
+score_when_first_player_scores_one_and_second_player_scores_three_test() ->
+    ?assertEqual({"fifteen", "forty"}, tennis:score(1,3)).
+
 score_when_both_players_score_one_test() ->
     ?assertEqual({"fifteen", "fifteen"}, tennis:score(1,1)).
 
@@ -34,6 +43,12 @@ score_when_first_player_score_four_and_second_player_scores_three_test() ->
 
 score_when_first_player_score_five_and_second_player_scores_five_test() ->
     ?assertEqual("deuce", tennis:score(5,5)).
+
+score_when_first_player_score_four_and_second_player_scores_zero_test() ->
+    ?assertEqual({"win", "first_player"}, tennis:score(4,0)).
+
+score_when_first_player_score_one_and_second_player_scores_four_test() ->
+    ?assertEqual({"win", "second_player"}, tennis:score(1,4)).
 
 score_when_first_player_score_four_and_second_player_scores_two_test() ->
     ?assertEqual({"win", "first_player"}, tennis:score(4,2)).
