48dfd9a6149ba119886feda4cec5326b6049ce96
savas <savasaydin@gmail.com>
2013-09-07 13:45:34 +0200

    refactored and put all numbers divisibly by ten into a function clause

--- number_names/src/number_names.erl
+++ number_names/src/number_names.erl
@@ -6,27 +6,11 @@ spell_out(0) ->
     "zero";
 spell_out(Number) when Number < 10  ->
     get_natural_number_name(Number);
-spell_out(10) ->
-    "ten";
 spell_out(Number) when Number > 10 andalso
 		       Number < 20 ->
     get_irregular_number_name(Number);
-spell_out(20) ->
-    "twenty";
-spell_out(30) ->
-    "thirty";
-spell_out(40) ->
-    "forty";
-spell_out(50) ->
-    "fifty";
-spell_out(60) ->
-    "sixty";
-spell_out(70) ->
-    "seventy";
-spell_out(80) ->
-    "eighty";
-spell_out(90) ->
-    "ninety";
+spell_out(Number) when Number rem 10 == 0 ->
+    lists:nth(Number div 10, tens());
 spell_out(Number) when Number < 100 ->
     LowerLimit = get_lower_limit_of_(Number),
     spell_out(LowerLimit) ++ " " ++ spell_out(Number-LowerLimit).
@@ -62,3 +46,14 @@ irregular_number_names() ->
      "nineteen"
     ].
 
+tens() ->    
+    ["ten",
+     "twenty",
+     "thirty",
+     "forty",
+     "fifty",
+     "sixty",
+     "seventy",
+     "eighty",
+     "ninety"
+    ].
