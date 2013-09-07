94a48903d6e4a3db341f05047a96b7d1d312d2fc
savas <savasaydin@gmail.com>
2013-09-07 12:09:04 +0200

    started over and handles number names up to 39

--- number_names/src/number_names.erl
+++ number_names/src/number_names.erl
@@ -2,83 +2,45 @@
 
 -export([spell_out/1]).
 
-spell_out(Number) ->
-    NumberNameFun = get_number_fun(Number),
-    NumberName = NumberNameFun(Number),
-    string:strip(NumberName).
+spell_out(0) ->
+    "zero";
+spell_out(Number) when Number > 0 andalso 
+		       Number < 10  ->
+    get_natural_number_name(Number);
+spell_out(10) ->
+    "ten";
+spell_out(Number) when Number > 10 andalso
+		       Number < 20 ->
+    get_irregular_number_name(Number);
+spell_out(20) ->
+    "twenty";
+spell_out(Number) when Number > 20 andalso
+		       Number < 30 ->
+    spell_out(20) ++ " " ++ spell_out(Number-20);
+spell_out(30) ->
+    "thirty";
+spell_out(Number) when Number > 30 andalso
+			Number < 40 ->
+    spell_out(30) ++ " " ++ spell_out(Number-30).
+					   
+get_natural_number_name(Number) ->
+    lists:nth(Number+1,first_nine_natural_number_names()).
 
-get_number_fun(Number) ->
-    proplists:get_value(is_irregular(Number), number_name_funs()).
-			 
-is_irregular(Number) ->
-    none =/= proplists:lookup(Number, irregular_number_names()).
+first_nine_natural_number_names() ->
+    ["zero","one","two","three","four","five","six","seven","eight","nine"].
 
-number_name_funs() ->
-    [{false, fun get_regular_number_name/1},
-     {true, fun get_irregular_number_name/1}
-    ].
-
-get_regular_number_name(Number) ->
-    [Hundreds, Tens, Ones] = parse_base_ten(Number, 100),
-    get_hundreds_name(Hundreds) ++ get_tens_name(Tens) ++ get_ones_name(Ones).
-
-parse_base_ten(_, 0) ->
-    [];
-parse_base_ten(Number, Base) ->
-    [Number div Base | parse_base_ten(Number rem Base, Base div 10)].
-  
-get_hundreds_name(0) ->
-    [];
-get_hundreds_name(Number) ->
-    proplists:get_value(Number, first_nine_natural_number_names()) ++ "hundred ".
-
-get_tens_name(0) ->
-    [];
-get_tens_name(Number) ->
-    proplists:get_value(Number, tens_names()).
-
-get_ones_name(0) ->
-    [];
-get_ones_name(Number) ->
-    proplists:get_value(Number, first_nine_natural_number_names()).
-
-get_irregular_number_name(Number) ->	
-    proplists:get_value(Number, irregular_number_names()).
+get_irregular_number_name(Number) ->
+    lists:nth(Number rem 10,irregular_number_names()).
 
 irregular_number_names() ->
-    [{0, "zero"},
-     {11, "eleven"},
-     {12, "twelve"},
-     {13, "thirteen"},
-     {14, "fourteen"},
-     {15, "fifteen"},
-     {16, "sixteen"},
-     {17, "seventeen"},
-     {18, "eighteen"},
-     {19, "nineteen"}
+    ["eleven",
+     "twelve",
+     "thirteen",
+     "fourteen",
+     "fifteen",
+     "sixteen",
+     "seventeen",
+     "eighteen",
+     "nineteen"
     ].
-   
-first_nine_natural_number_names() ->
-    [{1, "one "},
-     {2, "two "},
-     {3, "three "},
-     {4, "four "},
-     {5, "five "},
-     {6, "six "},
-     {7, "seven "},
-     {8, "eight "},
-     {9, "nine "}
-    ].
-    
-tens_names() ->
-    [{1, "ten "},
-     {2, "twenty "},
-     {3, "thirty "},
-     {4, "forty "},
-     {5, "fifty "},
-     {6, "sixty "},
-     {7, "seventy "},
-     {8, "eighty "},
-     {9, "ninety "}
-    ].
-    
+
