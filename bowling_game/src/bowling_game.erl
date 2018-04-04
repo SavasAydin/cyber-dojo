25bd7b6929fe42c16b3ec25edf810e2716e74ebe
Savas Aydin <savas.aydin@ericsson.com>
2018-04-04 07:55:29 +0200

    Converts '_' indicating a miss to zero hit

--- bowling_game/src/bowling_game.erl
+++ bowling_game/src/bowling_game.erl
@@ -11,6 +11,12 @@ parse_frames(Frames) ->
 
 to_hits([], Res) ->
     lists:reverse(Res);
+to_hits([[X, $-] | T], Acc) ->
+    Hit = {list_to_integer([X]), 0},
+    to_hits(T, [Hit | Acc]);
+to_hits([[$-, Y] | T], Acc) ->
+    Hit = {0, list_to_integer([Y])},
+    to_hits(T, [Hit | Acc]);
 to_hits([[X, Y] | T], Acc) ->
     Hit = {list_to_integer([X]), list_to_integer([Y])},
     to_hits(T, [Hit | Acc]).
--- bowling_game/test/bowling_game_tests.erl
+++ bowling_game/test/bowling_game_tests.erl
@@ -5,3 +5,13 @@ five_pins_hit_on_each_frame_of_all_ten_frames_test() ->
     TenFrames = "32|32|32|32|32|32|32|32|32|32||",
     Res = bowling_game:score(TenFrames),
     ?assertEqual(50, Res).
+
+nine_pins_hit_on_the_first_ball_of_all_ten_frames_test() ->
+    TenFrames = "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||",
+    Res = bowling_game:score(TenFrames),
+    ?assertEqual(90, Res).
+
+nine_pins_hit_on_the_second_ball_of_all_ten_frames_test() ->
+    TenFrames = "-9|-9|-9|-9|-9|-9|-9|-9|-9|-9||",
+    Res = bowling_game:score(TenFrames),
+    ?assertEqual(90, Res).
